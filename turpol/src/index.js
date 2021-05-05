85d77984dfd9cfc12787accb3a0847b325bff2c9
RobertDeSolti <hredil.r@gmail.com>
2021-05-05 19:32:03 +0300

    1

--- turpol/src/index.js
+++ turpol/src/index.js
@@ -1,14 +1,3 @@
-import './scss/style.scss'
-// import './section-header'
-// import './section-banner'
-// import './section-benefits'
-// import './section-inspiration'
-// import './section-perfection'
-// import './section-toplist'
-// import './section-video'
-// import './section-footer'
-// import './js/script'
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss/style.scss'
